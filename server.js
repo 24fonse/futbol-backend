d874c275be9cbe74d57718092d21b84aeb330988
Abdiel <fonsecapr@icloud.com>
2026-06-14 13:26:51 -0700

    agregar rutas PUT y DELETE

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ Conexión exitosa a MongoDB Atlas"))
 .catch(err => console.error("❌ Error de conexión:", err));
 
-// Esquema NoSQL de Mongoose
 const EquipoSchema = new mongoose.Schema({
   nombre: String,
   pais: String,
@@ -20,19 +18,25 @@ const EquipoSchema = new mongoose.Schema({
 });
 const Equipo = mongoose.model('Equipo', EquipoSchema);
 
-// RUTAS DE LA API
-
-// 1. GET: Obtener todos los equipos
 app.get('/equipos', async (req, res) => {
   const equipos = await Equipo.find();
   res.json(equipos);
 });
 
-// 2. POST: Insertar un nuevo equipo
 app.post('/equipos', async (req, res) => {
   const nuevoEquipo = new Equipo(req.body);
   await nuevoEquipo.save();
   res.json({ mensaje: "Equipo registrado", nuevoEquipo });
+});
+
+app.put('/equipos/:id', async (req, res) => {
+  const equipo = await Equipo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json({ mensaje: "Equipo actualizado", equipo });
+});
+
+app.delete('/equipos/:id', async (req, res) => {
+  await Equipo.findByIdAndDelete(req.params.id);
+  res.json({ mensaje: "Equipo eliminado" });
 });
 
 const PORT = process.env.PORT || 3000;
